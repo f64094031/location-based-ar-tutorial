f88ae48ca696c147290f121b5149d0bd3c05ab20
f64094031 <f64094031@gs.ncku.edu.tw>
2023-03-23 02:30:59 +0800

    new

--- script.js
+++ script.js
@@ -1,147 +1,68 @@
+// getting places from APIs
+function loadPlaces(position) {
+    const params = {
+        radius: 300,    // search places not farther than this value (in meters)
+        clientId: 'UBSA1TZKCUOKRVSJXRLVFBCOX54YVZENUCPAVWJ0KYSR0PMR',
+        clientSecret: 'C0WBWFJUSDTJCFSCK4EDRQJMLIFHJLMFIILXGFROY54VJZPC',
+        version: '20300101',    // foursquare versioning, required but unuseful for this demo
+    };
+
+    // CORS Proxy to avoid CORS problems
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API (limit param: number of maximum places to fetch)
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+        &ll=${position.latitude},${position.longitude}
+        &radius=${params.radius}
+        &client_id=${params.clientId}
+        &client_secret=${params.clientSecret}
+        &limit=30 
+        &v=${params.version}`;
+    return fetch(endpoint)
+        .then((res) => {
+            return res.json()
+                .then((resp) => {
+                    return resp.response.venues;
+                })
+        })
+        .catch((err) => {
+            console.error('Error with places API', err);
+        })
+};
+
+
 window.onload = () => {
-    const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    const scene = document.querySelector('a-scene');
 
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
 
-function staticLoadPlaces() {
-    return [
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
+
+                    // add place name
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
+
+                    scene.appendChild(placeText);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
         {
-			name: 'Pokèmon',
-            location: {
-                lat: 22.9988105,
-                lng: 120.2195032,
-            },
-			name: 'Pokèmon1',
-            location: {
-                lat: 22.998847, 
-                lng: 120.219269,
-            },
-			name: 'Pokèmon2',
-            location: {
-                lat: 22.998875, 
-                lng: 120.219199,
-            },
-			name: 'Pokèmon3',
-            location: {
-                lat: 22.9987477,
-                lng: 120.2190629,
-            },
-			name: 'Pokèmon4',
-            location: {
-                lat: 22.9986245,
-                lng: 120.2199156,
-            },
-			name: 'Pokèmon5',
-            location: {
-                lat: 22.998800, 
-                lng: 120.219826,
-            },
-			name: 'Pokèmon6',
-            location: {
-                lat: 23.008698, 
-                lng: 120.217373,
-            },
-			name: 'Pokèmon7',
-            location: {
-                lat: 23.008646, 
-                lng: 120.217468,
-            },
-			name: 'Pokèmon8',
-            location: {
-                lat: 23.008682, 
-                lng: 120.217468,
-            },
-			name: 'Pokèmon9',
-            location: {
-                lat: 23.008681, 
-                lng: 120.217412,
-            },
-			name: 'Pokèmon10',
-            location: {
-                lat: 23.008598,  
-                lng: 120.217547,
-            },
-        },
-    ];
-}
-
-var models = [
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
-    {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
-        info: 'Articuno, Lv. 80, HP 100/100',
-    },
-    {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
-        info: 'Dragonite, Lv. 99, HP 150/150',
-    },
-	{
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.06 0.06 0.06',
-        rotation: '0 180 0',
-        info: 'Bird, Lv. 999, HP 1500/1500',
-    },
-	{
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.8 0.8 0.8',
-        rotation: '0 180 0',
-        info: 'Snake, Lv. 1, HP 1/10',
-    },
-];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
-
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
-
-    entity.setAttribute('gltf-model', model.url);
-
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
 };
-
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-        setModel(models[modelIndex], model);
-
-        model.setAttribute('animation-mixer', '');
-
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
-    });
-}
