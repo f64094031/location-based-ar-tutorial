c836f0f2c984095455e99451ce7ec849e9b24779
f64094031 <f64094031@gs.ncku.edu.tw>
2023-03-22 11:52:08 +0800

    new

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function staticLoadPlaces() {
         {
             name: 'Magnemite',
             location: {
-                lat: 44.496470,
-                lng: 11.320180,
+                lat: 22.9986726,
+                lng: 120.2178136,
+            }
+			name: 'Magnemite1',
+            location: {
+                lat: 22.9886726,
+                lng: 120.2078136,
+            }
+			name: 'Magnemite2',
+            location: {
+                lat: 22.998652,
+                lng: 120.218399,
             }
         },
     ];
