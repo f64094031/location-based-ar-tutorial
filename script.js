adb227c53f88fb7641c866c2039ecf7e23128858
f64094031 <f64094031@gs.ncku.edu.tw>
2023-03-22 14:34:16 +0800

    new

--- script.js
+++ script.js
@@ -10,32 +10,32 @@ function staticLoadPlaces() {
             location: {
                 lat: 22.9986726,
                 lng: 120.2178136,
-            }
+            },
 			name: 'Magnemite1',
             location: {
                 lat: 22.9886726,
                 lng: 120.2078136,
-            }
+            },
 			name: 'Magnemite2',
             location: {
                 lat: 22.998652,
                 lng: 120.218399,
-            }
+            },
 			name: 'Magnemite3',
             location: {
                 lat: 22.9987477,
                 lng: 120.2190629,
-            }
+            },
 			name: 'Magnemite4',
             location: {
                 lat: 22.9986245,
                 lng: 120.2199156,
-            }
+            },
 			name: 'Magnemite5',
             location: {
                 lat: 22.998800, 
                 lng: 120.219826,
-            }
+            },
         },
     ];
 }
