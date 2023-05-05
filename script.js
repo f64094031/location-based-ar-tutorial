0443b36f44b2c22d7999257256c39ffb19dcd67a
f64094031 <f64094031@gs.ncku.edu.tw>
2023-05-05 23:05:49 +0800

    註解內容

--- script.js
+++ script.js
@@ -89,16 +89,16 @@ var models = [
         info: '1234',
     },*/
     {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
-    {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
+    },
+    {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Magnemite, Lv. 5, HP 10/10',
+        rotation: '0 180 0',
     },
     {
         url: './assets/dragonite/scene.gltf',
